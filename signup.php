e8e7c94be4b1475ee435b48f08d8893596083eef
Uros <urosivanovich@gmail.com>
2024-07-18 22:33:29 -0400

    adding an extra name input to have both, first and last name

--- signup.php
+++ signup.php
@@ -3,16 +3,20 @@
   <p class="lead text-center">It's free, and always will be</p>
   <form action="" class="mt-4 w-75">
     <div class="mb-3">
-      <label for="name" class="form-label">Full Name</label>
-      <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+      <label for="first_name" class="form-label">First Name</label>
+      <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Enter your first name">
+    </div>
+    <div class="mb-3">
+      <label for="last_name" class="form-label">Last Name</label>
+      <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Enter your last name">
     </div>
     <div class="mb-3">
       <label for="email" class="form-label">Email</label>
       <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
     </div>
     <div class="mb-3">
-        <label for="Password" class="form-label">Password</label>
-        <input type="Password" class="form-control" id="Password" name="Password" placeholder="Enter your password">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" name="password" placeholder="Enter your password">
     </div>
     <div class="mb-3">
       <input type="submit" name="submit" value="Sign Up" class="btn btn-dark w-100">
