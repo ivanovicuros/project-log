dc5e4b84c698d0160e5432c4d12cd4b812cfc504
Uros <urosivanovich@gmail.com>
2024-07-21 21:29:15 -0400

    setting up dsn

--- config/database.php
+++ config/database.php
@@ -1,8 +1,22 @@
 <?php
-$db_connection = pg_connect("host=localhost dbname=test user=urosivanovic password=password");
+// phpinfo();
+$host     = 'localhost';
+$port     = '5432';
+$user     = 'urosivanovic';
+$password = 'Bagzisha10$';
+$port     = '5432';
+$dbname   = 'test';
 
-if ($db_connection) {
-    echo "Connection to PostgreSQL database successful!";
-} else {
-    echo "Connection to PostgreSQL database failed!";
+// Set DSN
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+
+// Create a PDO instance
+$pdo = new PDO($dsn, $user, $password);
+
+# PDO QUERY
+
+$statement = $pdo->query('SELECT * FROM car');
+
+while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+    echo $row['model'] . '<br>';
 }
